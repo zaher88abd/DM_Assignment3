5d82b8fe06c56aba8f53928e2f57d4cdab0649e3
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-06-22 17:22:56 -0300

    create script for creating refactor the traning data

---
+++ refactorData.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_csv('Tweets.csv')
+# tweet_id,airline_sentiment,airline_sentiment_confidence,negativereason,negativereason_confidence,airline,
+# airline_sentiment_gold,name,negativereason_gold,retweet_count,text,tweet_coord,tweet_created,tweet_location,user_timezone
+
+# ItemID,Sentiment,SentimentText
+df = df.drop(['airline_sentiment_confidence', 'negativereason',
+              'negativereason_confidence', 'airline', 'airline_sentiment_gold', 'name',
+              'negativereason_gold', 'retweet_count', 'tweet_coord', 'tweet_created', 'tweet_location', 'user_timezone'], axis=1)
+df.columns = ['ItemID', 'Sentiment', 'SentimentText']
+df.to_csv('Tweets_new.csv', index=False)
