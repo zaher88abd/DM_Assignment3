33461411b621e29bf2ed88940daf97e4e26df163
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-06-22 16:51:53 -0300

    print the type of wordCount

--- spark_stream_listener.py
+++ spark_stream_listener.py
@@ -7,18 +7,21 @@ sc = SparkContext("local[2]", "Tweet Streaming App")
 
 ssc = StreamingContext(sc, 10)
 # sqlContext = SQLContext(sc)
-ssc.checkpoint( "file:/home/zaher/Github/DM_Assignment3/tweets/checkpoint/")
+ssc.checkpoint("file:/home/zaher/Github/DM_Assignment3/tweets/checkpoint/")
 
-socket_stream = ssc.socketTextStream("134.190.156.246", 9200) # Internal ip of  the tweepy streamer
+# Internal ip of  the tweepy streamer
+socket_stream = ssc.socketTextStream("134.190.156.246", 9200)
 
 lines = socket_stream.window(20)
 
-lines.count().map(lambda x:'Tweets in this batch: %s' % x).pprint()
+lines.count().map(lambda x: 'Tweets in this batch: %s' % x).pprint()
 # If we want to filter hashtags only
 # .filter( lambda word: word.lower().startswith("#") )
-words = lines.flatMap( lambda twit: twit.split(" ") )
-pairs = words.map( lambda word: ( word.lower(), 1 ) )
-wordCounts = pairs.reduceByKey( lambda a, b: a + b ) #.transform(lambda rdd:rdd.sortBy(lambda x:-x[1]))
+words = lines.flatMap(lambda twit: twit.split(" "))
+pairs = words.map(lambda word: (word.lower(), 1))
+# .transform(lambda rdd:rdd.sortBy(lambda x:-x[1]))
+wordCounts = pairs.reduceByKey(lambda a, b: a + b)
+print(type(wordCounts))
 wordCounts.pprint()
 
 ssc.start()
