db454f63ca4118ef6302a21ef988521e5f63ecf6
Mohamad Zaher Abd Ulmoula <zaher88abd@gmail.com>
2018-06-24 14:24:52 -0300

    change the code to sample code

--- load_model.py
+++ load_model.py
@@ -1,94 +1,65 @@
-from pyspark.sql import SQLContext
+from pyspark.ml import Pipeline, PipelineModel
+from pyspark.sql.functions import col
+from pyspark.sql import SQLContext, SparkSession
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
-from pyspark.sql.functions import col
-
-from pyspark.ml.feature import RegexTokenizer, StopWordsRemover, CountVectorizer
-from pyspark.ml.classification import LogisticRegression, NaiveBayes, LinearSVC
-
-from pyspark.ml import Pipeline, PipelineModel
-from pyspark.ml.feature import OneHotEncoder, StringIndexer, VectorAssembler
-
-from pyspark.ml.evaluation import BinaryClassificationEvaluator, MulticlassClassificationEvaluator
-from pyspark.sql import Row, SparkSession
-
+from collections import namedtuple
 # from pyspark.sql.functions import desc
-# print("Path",sys.argv[1].__str__())
 
-def getSparkSessionInstance(sparkConf):
-    if ('sparkSessionSingletonInstance' not in globals()):
-        globals()['sparkSessionSingletonInstance'] = SparkSession\
-            .builder\
-            .config(conf=sparkConf)\
-            .getOrCreate()
-    return globals()['sparkSessionSingletonInstance']
-
-
-def printer(x):
-    return "Tweet text: " + x
-
-
-sc = SparkContext("local[2]", "Tweet Streaming App")
+sc = SparkContext("local[2]", "Streaming App")
 
 
 ssc = StreamingContext(sc, 10)
-# sqlContext = SQLContext(sc)
-ssc.checkpoint("./tweets/checkpoint/")
+sqlContext = SQLContext(sc)
+#ssc.checkpoint( "file:/home/ubuntu/tweets/checkpoint/")
 
 # Internal ip of  the tweepy streamer
 socket_stream = ssc.socketTextStream("127.0.0.1", 9200)
-print("load model")
-lp = PipelineModel.load("piplineModel/")
-print("model  ", type(lp))
-
+lpModel = PipelineModel.load("piplineModel/")
 
 lines = socket_stream.window(20)
-print("Type of data", type(lines))
-
-# predictions = lp.transform(lines)
-# lines.map(lambda x: 'Tweets in this batch: %s' % x).pprint()
-# predictions['prediction'].map(lambda x: printer(x)).pprint()
-
-# Convert RDDs of the words DStream to DataFrame and run SQL query
+lines.pprint()
+fields = ("SentimentText")
+Tweet = namedtuple('Tweet', fields)
 
 
-def process(time, rdd):
+def getSparkSessionInstance(sparkConf):
+    if ("sparkSessionSingletonInstance" not in globals()):
+        globals()["sparkSessionSingletonInstance"] = SparkSession \
+            .builder \
+            .config(conf=sparkConf) \
+            .getOrCreate()
+    return globals()["sparkSessionSingletonInstance"]
+
+
+def do_something(time, rdd):
     print("========= %s =========" % str(time))
-
     try:
         # Get the singleton instance of SparkSession
         spark = getSparkSessionInstance(rdd.context.getConf())
 
-        # Convert RDD[String] to RDD[Row] to DataFrame
-        rowRdd = rdd.map(lambda w: Row(word=w))
-        wordsDataFrame = spark.createDataFrame(rowRdd)
+        # Convert RDD[String] to RDD[Tweet] to DataFrame
+        rowRdd = rdd.map(lambda w: Tweet(w))
+        linesDataFrame = spark.createDataFrame(rowRdd)
 
-        # Creates a temporary view using the DataFrame.
-        wordsDataFrame.createOrReplaceTempView("words")
-        print("Data...............")
-        print("Type of data 1 ", type(wordsDataFrame))
-        print("Type of data list ", wordsDataFrame.columns)
-        wordsDF = wordsDataFrame.withColumnRenamed('word', 'SentimentText')
-        print("Type of data list ", wordsDF.columns)
-        print("Type of data 1 ", type(wordsDF))
-        # print("model  ", lp)
-        # print("model  ", type(lp))
+        linesDataFrame = lpModel.transform(linesDataFrame)
 
-        # predictions = lp.transform(wordsDF)
-        # predictions.filter(predictions['prediction'] == 1) \
-        # .select("SentimentText", "Sentiment", "probability", "label", "prediction") \
-        # .orderBy("probability", ascending=False) \
-        # .show(n=10, truncate=30)
+        # Creates a temporary view using the DataFrame
+        linesDataFrame.createOrReplaceTempView("tweets")
 
-        # print("Type of data 2 ",type(predictions))
-        # # Do word count on table using SQL and print it
-        # wordCountsDataFrame = \
-        #     spark.sql("select word, count(*) as total from words group by word")
-        # wordCountsDataFrame.show()
+        # Do tweet character count on table using SQL and print it
+        lineCountsDataFrame = spark.sql(
+            "select SentimentText, prediction  from tweets")
+        print("Show data")
+        lineCountsDataFrame.show()
+        lineCountsDataFrame.write.format(
+            "com.databricks.spark.csv").save("dirwithcsv")
     except:
         pass
 
 
-lines.foreachRDD(process)
+# key part!
+lines.foreachRDD(do_something)
+
 ssc.start()
 ssc.awaitTermination()
